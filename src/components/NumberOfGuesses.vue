3a19b25d2f365e6a2a27e1140804ea03a7bf78d1
Margo Urey <margo@margonline.co.uk>
2023-10-28 18:44:21 +0100

    UI: experiment with circles for displaying number of guesses

--- src/components/NumberOfGuesses.vue
+++ src/components/NumberOfGuesses.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="banner">
+  <div class="container">
+    <div class="filled circle"></div>
+    <div class="filled circle"></div>
+    <div class="unfilled circle"></div>
+    <div class="unfilled circle"></div>
+  </div>
+  <div>
     <p>Number of guesses: {{ numOfGuesses() }}</p>
   </div>
 </template>
@@ -18,11 +24,20 @@ export default {
 <style scoped>
 .container {
   display: grid;
-  grid-template-columns: 1fr;
-  margin: 20px auto;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  margin: 25px auto;
+  padding: 0 40%;
 }
-.banner {
-  padding: 10px;
-  border-radius: 7px;
+.circle {
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  border: 1px solid black;
+}
+.filled {
+  background-color: grey;
+}
+.unfilled {
+  background-color: white;
 }
 </style>
